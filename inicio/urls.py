eb5aaed0205a144a84a14a962cbff03b13234be1
Giuliuss29 <giuliacosta29@hotmail.com>
2024-06-11 18:24:34 -0300

    v1.1.2
    Le agregue unos retoques y deje encaminado el backend

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import inicio, peliculas
+from .views import inicio, peliculas, series, famosos
 
 urlpatterns = [
     path('',inicio),
     path('/peliculas',peliculas,name='peliculas'),
+    path('/series',series,name='series'),
+    path('/famosos',famosos,name='famosos')
     
     
     
--- inicio/views.py
+++ inicio/views.py
@@ -1,8 +1,44 @@
 from django.shortcuts import render
+from inicio.forms import FormPelicula, FormSeries, FormFamosos
 
 def inicio(request):
     return render(request,'index.html' )
 
 def peliculas(request):
     return render(request,'peliculas.html' )
+    
+    if datos.method == 'POST':
+        formulario = FormPelicula(request.POST)
+        if formulario.is_valid():
+            datos = formulario.cleaned_data
+            pelicula = Peliucla(titulo=datos.POST.get('titulo'), director=datos.POST.GET('director'), genero=datos.POST.GET('genero'))
+            pelicula.save
+    formulario = FormPelicula()
+    return render(request,'inicio/peliculas.html',{'formulario':formulario})        
+
+
+def series(request):
+    return render(request,'series.html')
+    
+    if datos.method == 'POST':
+        formulario = FormSeries(request.POST)
+        if formulario.is_valid():
+            datos = formulario.cleaned_data
+            series = Serie(titulo=datos.POST.get('titulo'), director=datos.POST.GET('director'), genero=datos.POST.GET('genero'), temporadas=datos.Post.get('temporadas'))
+            series.save
+    formulario = FormSeries()
+    return render(request,'inicio/series.html',{'formulario':formulario}) 
+
+
+def famosos(request):
+    return render(request,'famosos.html')
+
+    if datos.method == 'POST':
+        formulario = FormFamosos(request.POST)
+        if formulario.is_valid():
+            datos = formulario.cleaned_data
+            famosos = Famosos(nombre=datos.POST.get('nombre'), profesion=datos.POST.GET('profesion'))
+            famosos.save
+    formulario = FormFamosos()
+    return render(request,'inicio/famosos.html',{'formulario':formulario}) 
     
